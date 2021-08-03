db72ecf664535357625f0b1949aee110a83393e8
foday <foday_s@yahoo.com>
2021-08-03 14:35:00 -0500

    Installation of react, material ui, axios and deletion of App.test.js, loge.svg, reportWebVitals.js, setupTests.js and went to App.js delete the import logo and the comments and other stuff below it. After everything deleted I test app by adding an hello world to index.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+
+  const dictionaryApi = { 
+    
+  }
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
