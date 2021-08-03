12f4f11eeb980bb826f29493dea3c742959779ed
foday <foday_s@yahoo.com>
2021-08-03 15:45:36 -0500

    created an dictionaryApi
    Created a dictionaryApi const and set up a try and catch object. The endpoint or dictionary api is stored in data with an axios so different browsers can talk to each other and to test it I sent the data to the console. useEffect is used so dictionaryApi method is rendered once because of [] dependency when the react app is rendered. useEffect does not need to be put in the return ().

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import axios from 'axios';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
 
-  const dictionaryApi = { 
-    
+  const dictionaryApi = async () => { 
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+
+      console.log(data);
+
+  } catch (error) { 
+    console.log(error);
   }
+};
+
+useEffect(() => { 
+  dictionaryApi();
+}, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
